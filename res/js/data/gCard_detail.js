25a0cabe8bf8719e521738b522b244184d7a4ed9
yJs <tearlight2008@gmail.com>
2014-12-10 17:38:20 +0800

    增加了贺语的选择

--- res/js/data/gCard_detail.js
+++ res/js/data/gCard_detail.js
@@ -45,3 +45,81 @@ gCardApp.run(['$location', '$rootScope', function($location, $rootScope) {
         }
     });
 }]);
+
+
+//监听事件
+$(document).on('click','.gCardShareBtn',function(){//分享按钮
+	var _gCardTips = $('#gCardTips')
+	;
+	
+	_gCardTips.css('display','block');
+	maskPad.show();
+})
+.on('click','.showMask',function(){//隐藏
+	var _gCardTips = $('#gCardTips')
+	;
+	_gCardTips.css('display','none');
+	maskPad.hide();
+
+	if(selectTextPad.enabled){
+		selectTextPad.hide();
+	}
+})
+.on('click','.gCardSTBtn',function(){
+	if(!selectTextPad.enabled){
+		selectTextPad.show();
+	}
+})
+.on('click','.gCardSTClose',function(){
+	selectTextPad.hide();
+})
+
+var selectTextPad = {
+	enabled:false
+	,show:function(){
+		var gCardSTCover = $('#gCardSTCover');
+		gCardSTCover.css('display','block');
+
+		maskPad.show();
+
+		this.enabled = true;
+		this.bindFn();
+	}
+	,hide:function(){
+		var gCardSTCover = $('#gCardSTCover');
+		gCardSTCover.css('display','none');
+
+		maskPad.hide();
+
+		this.enabled = false;
+		this.unbindFn();
+	}
+	,bindFn:function(){
+		var _self = this;
+		$('#gCardSTList').on('click','.itemSTCon',_self.selFn);
+	}
+	,unbindFn:function(){
+		var _self = this;
+		$('#gCardSTList').off('click','.itemSTCon',_self.selFn);
+	}
+	,selFn:function(){
+		var _this = $(this)
+			,_li = _this.parent()
+		;
+		if(_this.hasClass('on')){
+			_this.removeClass('on');
+		}else{
+			_this.addClass('on');
+			_li.siblings().children('.itemSTCon').removeClass('on');
+		}
+	}
+};
+
+var maskPad = {
+	show:function(){
+		$('#mask').css('display','block');
+	}
+	,hide:function(){
+		$('#mask').css('display','none');
+	}
+}
