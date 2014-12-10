63992fbfe7a1048926b560d9fd25c13a1b9a965a
yJs <tearlight2008@gmail.com>
2014-12-10 13:25:11 +0800

    增加配置文件

---
+++ res/js/config.js
@@ -0,0 +1,9 @@
+var config = {};
+config.action = {
+	cardIndex:'/res/json/cinfo.json'//贺卡管理首页
+	,cardCategory:'/res/json/category.json'//贺卡分类
+	,cardList:'/res/json/clist.json' //贺卡列表
+	,sendList:'/res/json/clist.json' //已发送
+	,receiveList:'/res/json/clist.json' //已接收
+	,cardDetail:'/res/json/cdetail.json'//详情
+}
