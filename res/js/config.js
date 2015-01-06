eb7bd107bdf891b51635440d5ca496ea9fecda12
yJs <tearlight2008@gmail.com>
2015-01-06 23:43:06 +0800

    增加basePath变量
    增加basePath变量，方便编写绝对路径

--- res/js/config.js
+++ res/js/config.js
@@ -1,4 +1,4 @@
-var config = {};
+var config = {},basePath,_location=window.location;
 config.action = {
 	cardIndex:'/res/json/cinfo.json'//贺卡管理首页
 	,cardCategory:'/res/json/category.json'//贺卡分类
@@ -7,3 +7,5 @@ config.action = {
 	,receiveList:'/res/json/clist.json' //已接收
 	,cardDetail:'/res/json/cdetail.json'//详情
 }
+basePath = _location.protocol+'//' + _location.host +':' + (_location.port || 80)+'/';
+_location = null;
