e93daed1554e74b563f3cddcbf992e4e87adb344
yJs <tearlight2008@gmail.com>
2015-01-09 22:59:08 +0800

    add new file

--- res/js/config.js
+++ res/js/config.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var config = {},basePath,_location=window.location;
+=======
+var config = {},utv={plugins:{}};
+>>>>>>> origin/master
 config.action = {
 	cardIndex:'/res/json/cinfo.json'//贺卡管理首页
 	,cardCategory:'/res/json/category.json'//贺卡分类
@@ -6,6 +10,143 @@ config.action = {
 	,sendList:'/res/json/clist.json' //已发送
 	,receiveList:'/res/json/clist.json' //已接收
 	,cardDetail:'/res/json/cdetail.json'//详情
+<<<<<<< HEAD
 }
 basePath = _location.protocol+'//' + _location.host +':' + (_location.port || 80)+'/';
 _location = null;
+=======
+	,friendList:'/res/json/friend.json' //好友列表
+}
+
+
+/*alert*/
+utv.plugins.alert = function(options){
+	var _def={
+		timeout:3000
+		,beforShow:function(){}
+		,afterClose:function(){}
+	}
+	,_opt = $.extend(_def,options)
+	;
+
+	this.opts = _opt;
+};
+utv.plugins.alert.prototype={
+	init:function(){
+		var _self = this
+			,_elem
+			,_winWidth = $(window).width()
+			,_div
+		;
+		var _divStr = [
+			'<div class="utv_alert">'
+			,'</div>'
+		].join('');
+		_div = $(_divStr);
+
+		$('body').append(_div);
+
+		_self.elem = _div;
+
+		_self.isInit = true;
+		_self.timer = null;
+		
+	}
+
+	//显示弹窗
+	,show:function(){
+		var _self = this
+			,_elem = _self.elem
+			,_width = 0
+			,_height = 0
+			,_win = $(window)
+			,_winWidth = _win.width()
+			,_winHeight = _win.height()
+			,_stop = _win.scrollTop()
+			,_opts = _self.opts
+			,_time = _opts.timeout
+		;
+		//_self.elem.fadeIn();
+		_elem.css({display:'block'});
+
+		_width = _elem.width();
+		_height = _elem.height();
+		_elem.css({left:(_winWidth-_width)/2,bottom:(_winHeight-_height)/2,display:'none'});
+
+		_self.elem.fadeIn();
+
+		clearTimeout(_self.timer);
+
+		_self.timer = setTimeout(function(){
+			_self.hide();
+		},_time);
+	}
+
+	//隐藏弹窗
+	,hide:function(){
+		var _self = this;
+		_self.elem.fadeOut();
+	}
+	,isInit:false
+	,alert:function(str){
+		var _self = this;
+
+		if(!_self.isInit){
+			_self.init();
+		}
+
+		_self.elem.html(str);
+		_self.show();
+	}
+};
+
+/*simple box*/
+utv.plugins.simBox = function(box,mask){
+	var _box = box,_mask = mask || $('#mask');
+
+	if(!_box) return false;
+
+	this.isInit = false;
+	this.box = _box;
+	this.mask = _mask;
+};
+utv.plugins.simBox.prototype={
+	show:function(){
+		var _self = this;
+		_self.box.show();
+		_self.mask.css('display','block');
+
+		if(!_self.isInit){
+			_self.initStyle();
+		}
+	}
+	,close:function(){
+		var _self = this;
+		_self.box.hide();
+		_self.mask.css('display','none');
+	}
+	,initStyle:function(){
+		var winWidth = $(window).width()
+			,_self = this
+			,docHeight = $(document).height()
+			,winHeight = $(window).height()
+			,boxHeight = _self.box.height()
+			,_mask = $('<div class="mask" id="mask"></div>')
+		;
+
+		if(_self.mask.length===0){
+			$('body').append(_mask);
+			_self.mask = _mask;
+			_mask.css('display','block');
+		}
+
+		_mask.css({height:docHeight});
+		_self.box.css({top:(winHeight-boxHeight)/2});
+
+		_self.isInit = true;
+		_self.box.on('tap click','.close',function(){
+			_self.close();
+		});
+	}
+};
+>>>>>>> origin/master
--- res/js/data/gCard_detail.js
+++ res/js/data/gCard_detail.js
@@ -6,6 +6,11 @@ gCardApp.config(['$routeProvider',function ($routeProvider) {
         ,title:'贺卡详情-UTVGO电视自由行'
         ,controller: 'cardDetail'
       })
+      .when('/send', {
+        templateUrl: 'res/tpl/gCard_detail_send.html'
+        ,title:'赠送贺卡-UTVGO电视自由行'
+        ,controller: 'cardDetailSend'
+      })
       .otherwise({
         redirectTo: '/'
       });
@@ -26,9 +31,113 @@ gCardApp.controller('cardDetail',function($scope,$http) {
         }
 
         $scope.data = _data;
+
+        //
+        selectTextPad._data={};
+        selectTextPad._data.img = _data.img;
+        selectTextPad._data.name = _data.name;
+        selectTextPad._data.text = _data.text;
     });
+
+    //监听事件
+	$(document).on('click','.gCardShareBtn',function(){//分享按钮
+		var _gCardTips = $('#gCardTips')
+		;
+		
+		_gCardTips.css('display','block');
+		maskPad.show();
+	})
+	.on('click','.gCardSTBtn',function(){
+		if(!selectTextPad.enabled){
+			selectTextPad.show();
+		}
+	})
+	.on('click','.gCardSTClose',function(){
+		selectTextPad.hide();
+	})
+	.on('click','.showMask',function(){
+		var _gCardTips = $('#gCardTips');
+		_gCardTips.css('display','none');
+		maskPad.hide();
+
+		if(selectTextPad.enabled){
+			selectTextPad.hide();
+		}
+	})
+	;
+	
+	var selectTextPad = {
+		enabled:false
+		,show:function(){
+			if(!this.box){
+				this.box = new utv.plugins.simBox($('#gCardSTCover'))
+			}
+			//
+			this.box.show();
+
+			this.enabled = true;
+			this.bindFn();
+		}
+		,hide:function(){
+			this.box.close();
+
+			this.enabled = false;
+			this.unbindFn();
+		}
+		,bindFn:function(){
+			var _self = this;
+			$('#gCardSTList').on('click','.itemSTCon',_self.selFn);
+		}
+		,unbindFn:function(){
+			var _self = this;
+			$('#gCardSTList').off('click','.itemSTCon',_self.selFn);
+		}
+		,selFn:function(){
+			var _this = $(this)
+				,_li = _this.parent()
+			;
+			if(_this.hasClass('on')){
+				_this.removeClass('on');
+			}else{
+				_this.addClass('on');
+				_li.siblings().children('.itemSTCon').removeClass('on');
+			}
+		}
+		,box:null
+	};
 });
 
+gCardApp.controller('cardDetailSend',function($scope,$http) {
+	$scope.data = selectTextPad._data;
+
+
+	$scope.update = function (data) {
+		$scope.friends = data; 
+		if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+		    $scope.$apply();
+		}
+	}
+
+	var box = new utv.plugins.simBox($('#gCardFD'))
+		,inputBox = new utv.plugins.simBox($('#inputKeyNo'))
+	;
+	friendPad.box = box;
+	friendPad.inputBox = inputBox;
+
+	$scope.selcetOwner = function(){
+		friendPad.getData(function(data){
+			$scope.update(data);
+			box.show();
+		});
+	}
+
+	$scope.addOwner = function(){
+		inputBox.show();
+		//prompt('请输入智能卡号或者广电号');
+	}
+});
+
+//过滤器
 gCardApp.filter('to_trusted', ['$sce', function($sce){
     return function(text) {
         return $sce.trustAsHtml(text);
@@ -46,74 +155,33 @@ gCardApp.run(['$location', '$rootScope', function($location, $rootScope) {
     });
 }]);
 
-
-//监听事件
-$(document).on('click','.gCardShareBtn',function(){//分享按钮
-	var _gCardTips = $('#gCardTips')
-	;
-	
-	_gCardTips.css('display','block');
-	maskPad.show();
-})
-.on('click','.showMask',function(){//隐藏
-	var _gCardTips = $('#gCardTips')
-	;
-	_gCardTips.css('display','none');
-	maskPad.hide();
-
-	if(selectTextPad.enabled){
-		selectTextPad.hide();
-	}
-})
-.on('click','.gCardSTBtn',function(){
-	if(!selectTextPad.enabled){
-		selectTextPad.show();
-	}
-})
-.on('click','.gCardSTClose',function(){
-	selectTextPad.hide();
-})
-
-var selectTextPad = {
-	enabled:false
-	,show:function(){
-		var gCardSTCover = $('#gCardSTCover');
-		gCardSTCover.css('display','block');
-
-		maskPad.show();
-
-		this.enabled = true;
-		this.bindFn();
-	}
-	,hide:function(){
-		var gCardSTCover = $('#gCardSTCover');
-		gCardSTCover.css('display','none');
-
-		maskPad.hide();
-
-		this.enabled = false;
-		this.unbindFn();
-	}
-	,bindFn:function(){
-		var _self = this;
-		$('#gCardSTList').on('click','.itemSTCon',_self.selFn);
-	}
-	,unbindFn:function(){
-		var _self = this;
-		$('#gCardSTList').off('click','.itemSTCon',_self.selFn);
-	}
-	,selFn:function(){
-		var _this = $(this)
-			,_li = _this.parent()
+var friendPad={
+	getData:function(cb){
+		var _url = config.action.friendList
+			,_self = this
 		;
-		if(_this.hasClass('on')){
-			_this.removeClass('on');
-		}else{
-			_this.addClass('on');
-			_li.siblings().children('.itemSTCon').removeClass('on');
+		/**/
+		if(_self.data){
+			cb(_self.data);
+			return false;
 		}
+
+		if(friendPad._req && friendPad._req.abort){
+			return false;
+		}
+
+
+		_self._req = $.getJSON(_url,function(data){
+			_self.data = data;
+			cb(data);
+			_self.req = null;
+		});
 	}
-};
+	,_req:null
+	,data:null
+	,box:null
+	,inputBox:null
+}
 
 var maskPad = {
 	show:function(){
